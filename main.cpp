d2ca50542eae539b8491bf60d95f7f3f588e37e0
MariaNov007 <novoselovam022006@mail.ru>
2025-11-25 19:00:32 +0500

    Каркас программы
    Добавлены 3 функции и вызов трех пустых функций

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-
+#include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
+
+// Функция чтения из файла в вектор
+void ReadFile(vector<string>& vector_stroka)
+{
+
+}
+
+// Функция вывода на экран
+void Print(vector<string>& vector_stroka)
+{
+
+}
+
+// Функция записи в файл
+void WriteFile(vector<string>& vector_stroka)
+{
+
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<string> vector_stroka;
+
+    ReadFile(vector_stroka);   // 1. Читаем
+    Print(vector_stroka);  // 2. Выводим на экран
+    WriteFile(vector_stroka);    // 3. Записываем в файл
+
+
     return 0;
 }
